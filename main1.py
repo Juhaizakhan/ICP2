f7eaa5a59c5da965c39612e1264faa4a4ddae827
Juhaizakhan <156458680+Juhaizakhan@users.noreply.github.com>
2024-06-03 22:30:23 -0500

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,12 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+star='*'
+for i in range(1,6):
+    for j in range (1,i):
+        print("*",end='')
+    print("\r")
+for i in range(6,1, -1):
+    for j in range(1,i):
+        print("*", end='')
+    print ("\r")
